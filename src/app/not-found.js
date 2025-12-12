3fec1ca1cfc4460ab3166612f29fdc92fec184b6
arhamshafi <arhambutt2923@gmail.com>
2025-12-13 01:16:37 +0500

    added pic on pagenot found or discover about router.replace

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function NOTFOUND() {
-  return (
-    <div className=''>
-      
-    </div>
-  )
+    const router = useRouter();
+
+    const goHome = () => {
+        router.replace("/");
+    };
+
+    return (
+        <div className='w-full min-h-screen flex flex-col relative justify-center items-center gap-5 text-black'>
+            <p className='font-bold text-2xl'>Error 404: This Route Drifted Off Orbit</p>
+            <p>Let’s Take You Back Home</p>
+
+            <button
+                onClick={goHome}
+                className='px-5 cursor-pointer py-2 bg-black text-white rounded-lg '
+            >
+                Home
+            </button>
+            <img src='/minions.png' className='absolute bottom-0 left-0 w-[300px] h-[300px] ' />
+        </div>
+    );
 }
