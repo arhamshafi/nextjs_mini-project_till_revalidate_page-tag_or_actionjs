06b579d77ac8d4dc5a1ef2ff40efe6fb3b84e954
arhamshafi <arhambutt2923@gmail.com>
2025-12-13 19:14:24 +0500

    added login sign up pages and reset there design

--- src/context/MainWrapper.jsx
+++ src/context/MainWrapper.jsx
@@ -12,7 +12,6 @@ export default function MainWrapper({ children, session }) {
                 <ToastContainer
                     position="top-center"
                     autoClose={1500}
-                    newestOnTop
                     closeOnClick
                     pauseOnHover
                     draggable
